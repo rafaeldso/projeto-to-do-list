65df38df96929bfcf6962e172554b6af74c18b2d
Rafael Oliveira <rafael_dso@hotmail.com>
2019-12-24 11:48:55 -0300

    Primeiro Commit

---
+++ to-do-list/src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="app">
+     <section class="hero is-primary is-bold is-fullheight">
+      <!-- Hero head: will stick at the top -->
+      <div class="hero-head">
+        <nav class="navbar">
+          <div class="container">
+            <div class="navbar-brand">
+              <a class="navbar-item">
+                To-do List
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+
+      <!-- Hero content: will be in the middle -->
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <div class="row">
+            <div class="columns">
+              <div class="column is-half is-offset-one-quarter">
+                <todo-card></todo-card>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Hero footer: will stick at the bottom -->
+      <div class="hero-foot">
+        <div class="container">
+          <div class="content has-text-centered">
+            <p>
+              <strong>To-do List</strong> by <a href="http://github.com/rafaeldso">Rafael Oliveira</a>.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </div>
+</template>
+
+<script>
+import TodoCard from './components/TodoCard'
+
+export default {
+  name: 'app',
+  components: {
+    TodoCard
+  },
+  data () {
+    return {
+      msg: 'Hello, World!'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ to-do-list/src/components/TodoCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title has-text-left">
+       Dia
+      </p>
+      <div class="has-text-right">
+        <p class="card-header-title">0 tarefas</p>
+      </div>
+    </header>
+    <div class="card-content">
+      <div class="content"></div>
+      <div class="content"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-card'
+}
+</script>
+
+<style>
+.card {
+  border-radius: 10px;
+}
+.card-header-title {
+  color: #636368;
+}
+</style>
